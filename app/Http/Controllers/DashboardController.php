5126f008552024cf8d4d7439d38b4fb88c8227db
Ahmed Nawaz <ahmedbhawana@gmail.com>
2021-12-28 14:49:29 +0500

    28-Dec

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,7 +8,9 @@ use App\Models\Doctor;
 use App\Models\Nurse;
 use App\Models\Patient;
 use App\Models\Staff;
+use App\Models\User;
 use Illuminate\Http\Request;
+use PhpParser\Comment\Doc;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -24,5 +26,41 @@ class DashboardController extends Controller
         $totalDepartment = Department::count();
         $totalAppointment = Appointment::count();
         return view('index',['appointmentCount'=>$totalAppointment,'count'=>$totalDoctor,'nursecount'=>$totalNurse,'patientCount'=>$totalPatient,'staffCount'=>$totalStaff,'departmentCount'=>$totalDepartment]);
+    }
+
+    public function exportCsv(Request $request){
+
+        $fileName = 'users.csv';
+        $tasks = User::all();
+
+        $headers = array(
+            "Content-type"        => "text/csv",
+            "Content-Disposition" => "attachment; filename=$fileName",
+            "Pragma"              => "no-cache",
+            "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+            "Expires"             => "0"
+        );
+
+        $columns = array('id', 'name', 'email', 'created_at', 'updated_at');
+
+        $callback = function() use($tasks, $columns) {
+            $file = fopen('php://output', 'w');
+            fputcsv($file, $columns);
+
+            foreach ($tasks as $task) {
+                $row['id']  = $task->id;
+                $row['name']    = $task->name;
+                $row['email']    = $task->email;
+                $row['created_at']  = $task->created_at;
+                $row['updated_at']  = $task->updated_at;
+
+                fputcsv($file, array($row['id'], $row['name'], $row['email'], $row['created_at'], $row['updated_at']));
+            }
+
+            fclose($file);
+
+        };
+
+        return response()->stream($callback, 200, $headers);
     }
 }
--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -33,23 +33,14 @@ class DoctorController extends Controller
     {
         $validated = $request->validated();
 
-        $validated =  Doctor::create([
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'title' => $request->title,
-            'address' => $request->address,
-            'experience' => $request->experience
-        ]);
+        $validated = Doctor::create(['first_name' => $request->first_name, 'last_name' => $request->last_name, 'email' => $request->email, 'phone' => $request->phone, 'title' => $request->title, 'address' => $request->address, 'experience' => $request->experience]);
         return response()->json(['success' => true, 'message' => 'Doctor Added Successfully!']);
     }
 
     public function datatable(Request $request)
     {
-        $params =[
-            'keyword'=>$request->keyword,
-        ];
+
+        $params = ['keyword' => $request->keyword,];
         $doctor = Doctor::select('*');
 
         if (isset($params['keyword']) && $params['keyword'] != '') {
@@ -61,22 +52,18 @@ class DoctorController extends Controller
         }
 
         if (request()->ajax()) {
-            return datatables()->of($doctor->orderBy('doctors.id','asc')->get())
-                ->addColumn('name',function ($data){
-                    return $data->first_name. ' '.$data->last_name;
-                })
-                ->addColumn('action', function ($data) {
+            return datatables()->of($doctor->orderBy('doctors.id', 'asc')->get())->addColumn('name', function ($data) {
+                    return $data->first_name . ' ' . $data->last_name;
+                })->addColumn('action', function ($data) {
 
                     $button = '<div style="width: 422px;"><a href="' . route('doctor.edit', $data->id) . '" class="d-inline-block"> <button type="button" name="edit"   class="edit mr-1 btn btn-primary btn-sm text-right">Edit</button></a>';
                     $button .= '&nbsp;&nbsp;';
                     $button .= '<a href="#" onclick="delete_doctor(' . $data->id . ')"  class="d-inline-block"><button type="button" name="delete"  class="delete  mr-1  btn btn-danger btn-sm text-right ">Delete</button></a>';
-                    $button .= '<a href="#" onclick="appointment('.$data->id.')"  class="d-inline-block"><button type="button" name="delete"  class="delete  mr-1  btn btn-danger btn-sm text-right ">Appointment</button></a>';
+                    $button .= '<a href="#" onclick="appointment(' . $data->id . ')"  class="d-inline-block"><button type="button" name="delete"  class="delete  mr-1  btn btn-danger btn-sm text-right ">Appointment</button></a>';
                     $button .= '<a href="' . route('doctor.patient.index', ['id' => $data->id]) . '" class="d-inline-block"><button type="button" name="delete" class="delete btn btn-dark btn-sm text-right ">Add Patient</button></a></div>';
                     return $button;
 
-                })
-                ->rawColumns(['action'])
-                ->make(true);
+                })->rawColumns(['action'])->make(true);
         }
 
     }
@@ -89,15 +76,8 @@ class DoctorController extends Controller
 
     public function update(Request $request)
     {
-        Validator::make($request->all(), [
-            'first_name' => 'required|regex:/^[a-zA-Z]+$/u|min:3|max:10',
-            'last_name' => 'required|regex:/^[a-zA-Z]+$/u|min:3|max:10',
-            'email' => 'required|email|unique:doctors,email,' . $request->id,
-            'phone' => 'required|min:10|unique:doctors,phone,' . $request->id,
-            'title' => 'required',
-            'address' => 'required',
-            'experience' => 'required'
-        ])->validate();
+
+        Validator::make($request->all(), ['first_name' => 'required|regex:/^[a-zA-Z]+$/u|min:3|max:10', 'last_name' => 'required|regex:/^[a-zA-Z]+$/u|min:3|max:10', 'email' => 'required|email|unique:doctors,email,' . $request->id, 'phone' => 'required|min:10|unique:doctors,phone,' . $request->id, 'title' => 'required', 'address' => 'required', 'experience' => 'required'])->validate();
 
         $doctor = Doctor::findorfail($request->id);
         $doctor->first_name = $request->first_name;
@@ -110,10 +90,12 @@ class DoctorController extends Controller
         $doctor->save();
 
         return response()->json(['success' => true, 'message' => 'Doctor Updated Successfully!']);
+
     }
 
     public function delete(Request $request)
     {
+
         $doctor = Doctor::find($request->id);
         $doctor->delete();
         return response()->json(['success' => true, 'message' => "Doctor Deleted!"]);
@@ -133,31 +115,21 @@ class DoctorController extends Controller
 
     public function patientdatatable(Request $request)
     {
-        $patient = Patient::join('doctor_patient', 'doctor_patient.patient_id', '=', 'patients.id')
-            ->where('doctor_patient.doctor_id', '=', $request->id)
-            ->select('patients.*');
+        $patient = Patient::join('doctor_patient', 'doctor_patient.patient_id', '=', 'patients.id')->where('doctor_patient.doctor_id', '=', $request->id)->select('patients.*');
 
-        return Datatables::of($patient->orderBy('doctor_patient.created_at', 'desc'))
-            ->addColumn('action', function ($data) {
+        return Datatables::of($patient->orderBy('doctor_patient.created_at', 'desc'))->addColumn('action', function ($data) {
                 $button = '<a href="#"> <button type="button" name="edit" onclick="deletePatient(' . $data->id . ');"   class="edit mr-1 btn btn-primary btn-sm text-right">Delete</button></a>';
                 return $button;
-            })
-            ->rawColumns(['action'])
-            ->make(true);
+            })->rawColumns(['action'])->make(true);
 
     }
 
     public function storePatient(Request $request)
     {
-        Validator::make($request->all(), [
-            'patient' => 'required|min:1',
-        ])->validate();
+        Validator::make($request->all(), ['patient' => 'required|min:1',])->validate();
 
         foreach ($request->patient as $key) {
-            DoctorPatient::create([
-                'patient_id' => $key,
-                'doctor_id' => $request->doctor_id
-            ]);
+            DoctorPatient::create(['patient_id' => $key, 'doctor_id' => $request->doctor_id]);
         }
         return response()->json(['success' => true, 'message' => 'Patient Added Successfully!']);
     }
@@ -169,8 +141,7 @@ class DoctorController extends Controller
         $patient = Patient::leftJoin('doctor_patient', function ($join) use ($doctor) {
             $join->on('doctor_patient.patient_id', '=', 'patients.id');
             $join->where('doctor_patient.doctor_id', '=', $doctor->id);
-        })->select('patients.id', 'patients.first_name', 'patients.last_name', 'patients.email')
-            ->whereNull('doctor_patient.doctor_id');
+        })->select('patients.id', 'patients.first_name', 'patients.last_name', 'patients.email')->whereNull('doctor_patient.doctor_id');
 
         // $patient = Patient::select('*');
         if ($request->has('search')) {
@@ -186,9 +157,7 @@ class DoctorController extends Controller
 
     public function deletePatient(Request $request)
     {
-        $patient = DoctorPatient::where('doctor_patient.doctor_id', '=', $request->doctor_id)
-            ->where('doctor_patient.patient_id', '=', $request->id)
-            ->delete();
+        $patient = DoctorPatient::where('doctor_patient.doctor_id', '=', $request->doctor_id)->where('doctor_patient.patient_id', '=', $request->id)->delete();
         if ($patient) {
             return response()->json(['success' => true, 'message', 'Patient Deleted']);
         } else {
@@ -197,20 +166,14 @@ class DoctorController extends Controller
     }
 
     public function viewAppointment(Request $request)
-{
-    $appointment = Appointment::select('*','patients.first_name as patient')
-        ->leftjoin('patients','patients.id','=','appointments.patient_id')
-        ->where('appointments.doctor_id','=',$request->id);
-
-    if(request()->ajax())
     {
-        return datatables()->of($appointment->orderBy('appointments.id','asc')->get())
-            ->addColumn('action', function($data){
+        $appointment = Appointment::select('*', 'patients.first_name as patient')->leftjoin('patients', 'patients.id', '=', 'appointments.patient_id')->where('appointments.doctor_id', '=', $request->id);
 
-            })
-            ->rawColumns(['action'])
-            ->make(true);
+        if (request()->ajax()) {
+            return datatables()->of($appointment->orderBy('appointments.id', 'asc')->get())->addColumn('action', function ($data) {
+
+                })->rawColumns(['action'])->make(true);
+        }
     }
-}
 
 }
